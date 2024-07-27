423d980b12e2e86b5bc3981b97d4995ba61d5d1e
Kevin Suñer <ksuner@pm.me>
2024-07-27 18:30:55 +0200

    feat(core): initialize gtk-rs project with hello-world

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use gtk::{glib, Application};
+use gtk::{prelude::*, ApplicationWindow, Button};
+
+const APP_ID: &str = "github.kevinsuner.Fontibus";
+
+fn main() -> glib::ExitCode {
+    let app = Application::builder().application_id(APP_ID).build();
+    app.connect_activate(build_ui);
+    app.run()
+}
+
+fn build_ui(app: &Application) {
+    let button = Button::builder()
+        .label("Press me!")
+        .margin_top(12)
+        .margin_bottom(12)
+        .margin_start(12)
+        .margin_end(12)
+        .build();
+
+    button.connect_clicked(|button| button.set_label("Hello world!"));
+
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("Fontibus")
+        .child(&button)
+        .build();
+
+    window.present();
+}
